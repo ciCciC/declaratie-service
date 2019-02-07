93a7aeb6975d246dcf4d9e99c298e77e05ec6023
ciCic <koraypoyraz1@gmail.com>
2019-02-07 15:59:27 +0100

    Added security configuration

---
+++ declaratie-api/src/main/java/com/declaratie/declaratieapi/config/SecurityConfig.java
@@ -0,0 +1,39 @@
+package com.declaratie.declaratieapi.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+
+        auth.inMemoryAuthentication()
+                .withUser("admin")
+                .password(encoder.encode("admin123"))
+                .roles("ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests()
+                .anyRequest()
+                .fullyAuthenticated()
+                .and()
+                .formLogin()
+                .and()
+                .httpBasic();
+
+        http.csrf().disable();
+
+    }
+}
